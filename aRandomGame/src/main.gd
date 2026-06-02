572c0390dc42344b889742122d1606e110cc6a51
Lohan <lohan.marchand@etu.unistra.fr>
2026-06-02 21:12:13 +0200

    création de home_menu et loading_screen

--- aRandomGame/src/main.gd
+++ aRandomGame/src/main.gd
@@ -2,3 +2,9 @@ extends Node
 
 # La scène main sert de scène de base dès le démarrage du jeu.
 # Elle lancera d'abord une scène main_menu puis gèrera le changement vers the_game
+@onready var game : PackedScene = preload("res://scenes/game/the_game.tscn")
+
+func _ready() -> void:
+	# à déplacer plus tard ⬇️
+	var game_instance = game.instantiate()
+	add_child(game_instance)
